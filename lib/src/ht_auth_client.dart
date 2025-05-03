2cd4ab7e9162c25b358ac6063be502dab2fa7e4c
fulleni <fulleni@hotmail.com>
2025-05-03 08:33:34 +0100

    feat: add initial project setup and configurations
    - Create basic project structure and files
    - Add issue templates and pull request template
    - Configure CI/CD, spell check, and dependency management
    - Set up analysis options and ignore files
    - Implement basic library functionality and initial test

---
+++ lib/src/ht_auth_client.dart
@@ -0,0 +1,7 @@
+/// {@template ht_auth_client}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtAuthClient {
+  /// {@macro ht_auth_client}
+  const HtAuthClient();
+}
---
+++ test/src/ht_auth_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_auth_client/ht_auth_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAuthClient', () {
+    test('can be instantiated', () {
+      expect(HtAuthClient(), isNotNull);
+    });
+  });
+}
