821be6527806590f14cae998136630dcb8886587
fulleni <fulleni@hotmail.com>
2025-05-03 08:55:13 +0100

    test: switch to mocktail and add code coverage
    - Replace mockito with mocktail for creating mock instances
    - Add initial code coverage reporting with lcov.info file
    - Update test to use mock HtAuthClient instance

--- test/src/ht_auth_client_test.dart
+++ test/src/ht_auth_client_test.dart
@@ -1,11 +1,15 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:ht_auth_client/ht_auth_client.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+
+// Create a mock implementation of HtAuthClient using mocktail
+class MockHtAuthClient extends Mock implements HtAuthClient {}
 
 void main() {
   group('HtAuthClient', () {
-    test('can be instantiated', () {
-      expect(HtAuthClient(), isNotNull);
+    test('mock can be instantiated', () {
+      // Test that a mock instance can be created, confirming the interface setup
+      expect(MockHtAuthClient(), isNotNull);
     });
   });
 }
